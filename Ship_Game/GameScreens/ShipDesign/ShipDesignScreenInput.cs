b67c57b8f95e03154f74d6a0f529c5620931fe18
Agamenton <ofgagamenton@seznam.cz>
2023-12-28 17:06:30 +0100

    fixed issue created by previous commit: 6cb2be945e12763c20c7c2507279a9496c616ea2

--- Ship_Game/GameScreens/ShipDesign/ShipDesignScreenInput.cs
+++ Ship_Game/GameScreens/ShipDesign/ShipDesignScreenInput.cs
@@ -396,7 +396,7 @@ namespace Ship_Game
             if (!input.RightMouseClick)
                 return;
 
-            if (slotUnderCursor != null && slotUnderCursor.Parent != null)
+            if (slotUnderCursor != null && (slotUnderCursor.Module != null || slotUnderCursor.Parent != null))
                 DeleteModuleAtSlot(slotUnderCursor);
             else
                 ActiveModule = null;
